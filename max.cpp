a7433b8ae56ab848f3f3103a4baacbb6ae6c7fe2
senthilmurugan41 <54075604+senthilmurugan41@users.noreply.github.com>
2019-08-13 22:47:41 +0530

    Create max.cpp

---
+++ max.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int* a = new int[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    sort(a,a+n);
+    cout<<a[n-1];
+    return 0;
+}

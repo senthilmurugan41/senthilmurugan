ddfc57dea3e6b4a1e9bd63bd6677531034eeb86e
senthilmurugan41 <54075604+senthilmurugan41@users.noreply.github.com>
2019-08-13 22:20:06 +0530

    Create Odd.cpp

---
+++ Odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n;
+   if(n<0)
+     cout<<"invalid";
+   if(n%2==0)
+      cout<<"Even";
+   else
+       cout<<"Odd";
+    return 0;
+ }
+       

04a51f5c0c9051597e28b0dae68e132754d1e9e7
senthilmurugan41 <54075604+senthilmurugan41@users.noreply.github.com>
2019-08-15 21:33:14 +0530

    Create digitword.cpp

---
+++ digitword.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   string a[]={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
+   int n;
+   cin>>n;
+   cout<<a[n-1];
+}

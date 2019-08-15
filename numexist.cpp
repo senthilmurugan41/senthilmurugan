2f086f404b117314af8dd8ae1ebe35338f0e97f5
senthilmurugan41 <54075604+senthilmurugan41@users.noreply.github.com>
2019-08-15 22:00:28 +0530

    Create numexist.cpp

---
+++ numexist.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n,k,a[1000],c=0;
+  cin>>n>>k;
+  for(int i=0;i<n;i++)
+    cin>>a[i];
+  for(int i=0;i<n;i++)
+  {
+      if(a[i]==k)
+        c++;
+  }
+  if(c!=0)
+    cout<<"yes";
+  else
+    cout<<"no";
+}

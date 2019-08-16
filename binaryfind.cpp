cd0c7a71066a1928220755283e24bf4c4f50779d
senthilmurugan41 <54075604+senthilmurugan41@users.noreply.github.com>
2019-08-16 11:56:25 +0530

    Create binaryfind.cpp

---
+++ binaryfind.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,flag=0;
+    cin>>n;
+    while(n!=0)
+    {
+        if((n%10)*2==0||(n%10)*2==2)
+           flag=1;
+        else
+            flag=0;
+        n=n/10;
+    }
+    if(flag!=0)
+        cout<<"yes";
+    else
+        cout<<"no";
+}
+

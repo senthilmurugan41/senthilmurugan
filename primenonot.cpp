bcbadc7ca469018397a0d69edf4ec1f6e0547bfe
senthilmurugan41 <54075604+senthilmurugan41@users.noreply.github.com>
2019-08-16 12:09:07 +0530

    Create primenonot.cpp

---
+++ primenonot.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n,flag=0;
+   cin>>n;
+   for(int i=2;i<n;i++)
+   {
+       if(n%i==0)
+         flag=1;
+   }
+   if(flag==0)
+    cout<<"yes";
+   else
+    cout<<"no";
+}
+

067792ba9466b5715f78125b22513fc539c29a59
senthilmurugan41 <54075604+senthilmurugan41@users.noreply.github.com>
2019-08-17 23:19:58 +0530

    Create upper.cpp

---
+++ upper.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin,s);
+    for(int i=0;i<s.length();i++)
+    {
+        cout<<char(toupper(s[i]));
+    }
+
+}
+
